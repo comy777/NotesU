c28b238301cdbfe606ac17c1076afe45dda9a8c5
Sebastian Carrillo <carrillosebastian1@gmail.com>
2023-04-25 12:45:28 -0500

    Chore added eas build

--- app.config.js
+++ app.config.js
@@ -5,6 +5,12 @@ module.exports = {
   version: '1.0.0',
   extra: {
     apiUrl: process.env.API_URL,
+    eas: {
+      projectId: "562f057e-ce5b-4571-8033-20e94232fec8"
+    }
   },
-  userInterfaceStyle: "automatic"
+  userInterfaceStyle: "automatic",
+  android: {
+    package: "com.comy777.notesU",
+  }
 };
